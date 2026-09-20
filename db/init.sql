926c4e611e6d9372e5a5712169fb227c5abbc4f3
cristycontreras <cmcontrerasc@fi.uba.ar>
2026-09-20 17:53:00 -0300

    hago unas correciones en el .sql

--- db/init.sql
+++ db/init.sql
@@ -33,10 +33,15 @@ CREATE TABLE IF NOT EXISTS reservas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_cancha INT NOT NULL,
     id_socio INT NOT NULL,
+    /* dejo comentado para marcar la correccion
     fecha DATE NOT NULL,
     hora_inicio TIME NOT NULL,
     hora_fin TIME NOT NULL,
-    monto_total DECIMAL(10, 2) NOT NULL,
+    monto_total DECIMAL(10, 2) NOT NULL,*/
+    fecha_hora_inicio DATETIME(6) NOT NULL,
+    fecha_hora_fin DATETIME(6) NOT NULL,
+    precio_hora DECIMAL(10, 2) NOT NULL,
+    precio_total DECIMAL(10, 2) NOT NULL,
     estado ENUM('confirmada', 'cancelada', 'completada') NOT NULL DEFAULT 'confirmada',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_reservas_socio FOREIGN KEY (id_socio) REFERENCES socios(id) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -47,4 +52,5 @@ CREATE TABLE IF NOT EXISTS reservas (
 -- Datos iniciales de prueba para reservas
 INSERT INTO reservas (id_cancha, id_socio, fecha, hora_inicio, hora_fin, monto_total, estado)
 VALUES 
-(1, 1, '2026-09-20', '18:00:00', '19:00:00', 15000.00, 'confirmada');
+(1, 1, '2026-10-20 18:00:00.000000', '2026-10-20 19:00:00.000000', 15000.00, 15000.00, 'confirmada');
+-- (1, 1, '2026-09-20', '18:00:00', '19:00:00', 15000.00, 'confirmada');
